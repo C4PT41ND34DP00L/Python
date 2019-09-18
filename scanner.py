379b354561d813c46300846448aa0d968159e216
Jacob T Lusco <lusco.jacob@gmail.com>
2019-09-18 11:04:22 -0600

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+from socket import *
+import sys, time
+from datetime import datetime
+
+host = ''
+max_port = 5000
+min_port = 1
+
+def scan_host(host, port, r_code = 1):
+	try:
+		s = socket(AF_INT, SOCK_STREAM)
+		code = s.connect_ex((host, port))
+		if code ==0:
+			r_code = code
+		s.close()
+	except Exception, e:
+		pass
+	return r_code
+
+try:
+	host = input("[*] Enter Target Host Address")
+except KeyboardInterrupt:
+	print("\n\n[*] User Requested an Interrupt.")
+	print("[*] Application Shutting Down.")
+	sys.exit(1)
+
+hostip = gethostbyname(host)
+print("\n[*] Host: %s IP: %s" % (host, hostip))
+print("[*] Scanning Started at %s...\n" % (time.steftime("%H:%M:%s)))
+start_time = datetime.now()
+
+for port in range(min_port, max_port):
+	try:
+		response = scan_host(host, port)
+
+		if response == 0:
+			print("[+] Port %d: Open" % (port))
+	except Exception, e:
+		pass
+stop_time = datetime.now()
+total_time_ duration = stop_time - start_time
+print("\n[*] Scannig Finished at %s ..." % (time.strftime("%H:%M:%s")))
+print("[*] Scanning Duration: %s ..." % (total_time_duration))
+print("[*] Have a nice day ... Sergeant Exploiter ( Sploit )") 
